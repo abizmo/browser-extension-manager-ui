3012164d1ae0371c6af4674e10162a99aa5128b0
abizmo <8437730+abizmo@users.noreply.github.com>
2025-05-18 11:36:59 +0100

    chore: add preact

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,10 @@ import { defineConfig } from 'astro/config';
 
 import vercel from '@astrojs/vercel';
 
+import preact from '@astrojs/preact';
+
 // https://astro.build/config
 export default defineConfig({
-  adapter: vercel()
+  adapter: vercel(),
+  integrations: [preact()]
 });
